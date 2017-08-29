2c65d34b8d60009207f59424f75baf12911297f5
Shravan Rajinikanth <shravan2x@gmail.com>
2017-08-28 20:25:22 -0500

    Initial commit

---
+++ Gameloop.Vdf.JsonConverter/JTokenExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace Gameloop.Vdf.JsonConverter
+{
+    public static class JTokenExtensions
+    {
+        public static VToken ToVdf(this JToken tok)
+        {
+            switch (tok)
+            {
+                case JValue val:
+                    return val.ToVdf();
+                case JProperty prop:
+                    return prop.ToVdf();
+                case JObject obj:
+                    return obj.ToVdf();
+                case JArray arr:
+                    return arr.ToVdf();
+                default:
+                    throw new InvalidOperationException("Unrecognized JToken.");
+            }
+        }
+
+        public static VValue ToVdf(this JValue val)
+        {
+            return new VValue(val.Value);
+        }
+
+        public static VProperty ToVdf(this JProperty prop)
+        {
+            return new VProperty(prop.Name, prop.Value.ToVdf());
+        }
+
+        public static VObject ToVdf(this JObject obj)
+        {
+            VObject resultObj = new VObject();
+
+            foreach (JToken tok in obj.Children())
+                resultObj.Add((VProperty) tok.ToVdf());
+
+            return resultObj;
+        }
+
+        public static VObject ToVdf(this JArray arr)
+        {
+            VObject resultObj = new VObject();
+
+            for (int index = 0; index < arr.Count; index++)
+                resultObj.Add(index.ToString(), arr[index].ToVdf());
+
+            return resultObj;
+        }
+    }
+}
---
+++ Gameloop.Vdf.JsonConverter/VTokenExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace Gameloop.Vdf.JsonConverter
+{
+    public static class VTokenExtensions
+    {
+        public static JToken ToJson(this VToken tok)
+        {
+            switch (tok)
+            {
+                case VValue val:
+                    return val.ToJson();
+                case VProperty prop:
+                    return prop.ToJson();
+                case VObject obj:
+                    return obj.ToJson();
+                default:
+                    throw new InvalidOperationException("Unrecognized VToken.");
+            }
+        }
+
+        public static JValue ToJson(this VValue val)
+        {
+            return new JValue(val.Value);
+        }
+
+        public static JProperty ToJson(this VProperty prop)
+        {
+            return new JProperty(prop.Key, prop.Value.ToJson());
+        }
+
+        public static JObject ToJson(this VObject obj)
+        {
+            JObject resultObj = new JObject();
+
+            foreach (VProperty prop in obj.Children())
+                resultObj.Add(prop.ToJson());
+
+            return resultObj;
+        }
+    }
+}
